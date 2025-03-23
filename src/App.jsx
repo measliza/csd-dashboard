edbf0ea1dc21907b564bbf54795a7e1fc3b221ca
Khim070 <Vakhiml60@gmail.com>
2025-03-23 21:05:26 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-// import Home from './pages/home';
 import Menu from './pages/Menu';
 import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import Login from './pages/Login';
@@ -12,11 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={<Navigate to="/menu"/>}/>
-        {/* <Route path='/home' element={<Home/>} /> */}
         <Route path='/menu' element={<Menu/>} />
-
         <Route path='/image' element={<Image/>} />
-
         <Route path='/login' element={<Login/>} />
       </Routes>
     </Router>
